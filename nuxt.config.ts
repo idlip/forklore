13863db6b1a8cb98f343f1b56b5ea2cef3e6cc12
Dilip <igoldlip@gmail.com>
2025-10-06 21:00:57 +0530

    ssr one

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,
+  ssr: true,
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
   modules: ["@nuxt/content", "@nuxtjs/color-mode"],
