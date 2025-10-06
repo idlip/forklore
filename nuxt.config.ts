974accd9fbfd3e15152a39e2416295e99cd8b7ec
Dilip <igoldlip@gmail.com>
2025-10-06 21:15:39 +0530

    fix ssg

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,
+  ssr: true,
   devtools: { enabled: true },
   modules: ["@nuxt/content", "@nuxtjs/color-mode"],
   css: ["~/assets/css/main.css"],
@@ -30,17 +30,10 @@ export default defineNuxtConfig({
       ],
     },
   },
-  routeRules: {
-    "/": { prerender: false },
-  },
   compatibilityDate: "2025-05-15",
   nitro: {
-    preset: "cloudflare_module",
-    cloudflare: {
-      deployConfig: true,
-      nodeCompat: true
-    },
   prerender: {
+      crawlLinks: true,
       routes: ['/rss']
     }
   }
